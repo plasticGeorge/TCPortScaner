090a9014f7bb9dda23fd94336f8c9db4c3871b78
plasticGeorge <georgylemko@yandex.ru>
2021-05-13 21:05:14 +0300

    first commit. half of work comleted. required add multithreading in ports scaner

---
+++ src/main/java/com/plasticgeorge/tcpscaner/ArgumentsProcessor.java
@@ -0,0 +1,5 @@
+package com.plasticgeorge.tcpscaner;
+
+public interface ArgumentsProcessor {
+    void processArguments(String[] args);
+}
---
+++ src/main/java/com/plasticgeorge/tcpscaner/DefaultArgumentsProcessor.java
@@ -0,0 +1,8 @@
+package com.plasticgeorge.tcpscaner;
+
+public class DefaultArgumentsProcessor implements ArgumentsProcessor{
+    @Override
+    public void processArguments(String[] args) {
+
+    }
+}
